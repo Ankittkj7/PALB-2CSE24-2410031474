b797f6b1625b0de1ff19c501945272535bbb8501
Ankitkj7 <155059951+Ankittkj7@users.noreply.github.com>
2026-02-13 09:50:32 +0530

    Update Program17.py

--- Homework assignment/Program17.py
+++ Homework assignment/Program17.py
@@ -1,49 +1,23 @@
-def commonElements(arr1, arr2, arr3):
-    i = j = k = 0
-    result = []
+def factorialDigits(n):
+    result = [1]   # stores digits of factorial
+    size = 1       # number of digits
 
-    while i < len(arr1) and j < len(arr2) and k < len(arr3):
-        # If all three are equal
-        if arr1[i] == arr2[j] == arr3[k]:
-            # Add only once
-            if not result or result[-1] != arr1[i]:
-                result.append(arr1[i])
+    for x in range(2, n + 1):
+        carry = 0
+        for i in range(size):
+            prod = result[i] * x + carry
+            result[i] = prod % 10
+            carry = prod // 10
 
-            # Skip duplicates
-            val = arr1[i]
-            while i < len(arr1) and arr1[i] == val:
-                i += 1
-            while j < len(arr2) and arr2[j] == val:
-                j += 1
-            while k < len(arr3) and arr3[k] == val:
-                k += 1
+        while carry > 0:
+            result.append(carry % 10)
+            carry //= 10
+            size += 1
 
-        # Move pointer with smallest value
-        elif arr1[i] < arr2[j]:
-            i += 1
-        elif arr2[j] < arr3[k]:
-            j += 1
-        else:
-            k += 1
-
-    return result if result else [-1]
+    return result[::-1]   # reverse to correct order
 
 
-# Examples
-print(commonElements(
-    [1, 5, 10, 20, 40, 80],
-    [6, 7, 20, 80, 100],
-    [3, 4, 15, 20, 30, 70, 80, 120]
-))
-
-print(commonElements(
-    [1, 2, 3, 4, 5],
-    [6, 7],
-    [8, 9, 10]
-))
-
-print(commonElements(
-    [1, 1, 1, 2, 2, 2],
-    [1, 1, 2, 2, 2],
-    [1, 1, 1, 1, 2, 2, 2, 2]
-))
+# Test cases
+print(factorialDigits(5))     # [1, 2, 0]
+print(factorialDigits(10))    # [3, 6, 2, 8, 8, 0, 0]
+print(factorialDigits(1))     # [1]
