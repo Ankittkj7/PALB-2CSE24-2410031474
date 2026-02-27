cd532ac8f8fa254887ef93f78c1eefc42d20bd31
Ankitkj7 <ankit.kumar4.cs28@iilm.edu>
2026-02-27 09:56:41 +0530

    Update Program13.py

--- Homework assignment/Program13.py
+++ Homework assignment/Program13.py
@@ -4,7 +4,6 @@ def minJumps(arr):
     if n <= 1:
         return 0
 
-    # If first element is 0, we cannot move
     if arr[0] == 0:
         return -1
 
@@ -14,19 +13,14 @@ def minJumps(arr):
     steps = arr[0]
 
     for i in range(1, n):
-        # If we have reached the last index
+        
         if i == n - 1:
             return jumps
-
-        # Update maximum reach
         max_reach = max(max_reach, i + arr[i])
         steps -= 1
-
-        # If no steps remain
         if steps == 0:
             jumps += 1
 
-            # If current position is not reachable
             if i >= max_reach:
                 return -1
 
@@ -39,3 +33,4 @@ def minJumps(arr):
 # Example
 arr = [1, 3, 5, 8, 9, 2, 6, 7, 6, 8, 9]
 print(minJumps(arr)) 
+
